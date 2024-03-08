b797757d3d0dd803ab15356a7fb7ff89803fd806
SergeiT <uraniumcore238@gmail.com>
2024-03-08 21:10:20 +0200

    fixes by comments

--- tests/level_2/test_three_first.py
+++ tests/level_2/test_three_first.py
@@ -3,17 +3,17 @@ import pytest
 from functions.level_2.three_first import first
 
 
-@pytest.mark.parametrize(('items', 'default', 'result'), [
+@pytest.mark.parametrize(('items', 'default', 'expected'), [
     ([1, 2], 4, 1),
     ([1, 2], None, 1),
     ([1, 2], 'NOT_SET', 1),
     ([], 5, 5),
     ([], None, None)
 ])
-def test_with_return_value(items, default, result):
-    assert first(items, default) == result
+def test__three_first__with_return_value(items, default, expected):
+    assert first(items, default) == expected
 
 
-def test_with_attribute_error():
+def test__three_first__attribute_error_if_default_equals_not_set_and_no_items():
     with pytest.raises(AttributeError):
         assert first([], 'NOT_SET') == 0
