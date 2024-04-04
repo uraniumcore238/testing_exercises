42a2aab5a1c7fde8ecefe8f5d350ea4ec358c483
SergeiT <uraniumcore238@gmail.com>
2024-04-04 19:21:57 +0300

    level_4 tests

--- tests/level_4/conftest.py
+++ tests/level_4/conftest.py
@@ -14,8 +14,8 @@ def file_path():
 
 @pytest.fixture
 def file_path_config():
-    path_to_file = 'test_config.txt'
+    path_to_file = 'test.txt'
     with open(path_to_file, 'w') as file_handler:
-        file_handler.writelines(['[tool:app-config]\n', 'field1=value1\n', 'field2=value2\n'])
+        file_handler.writelines(['[tool:app-config]\n', 'extra_fields = field1: int\n', 'field2: str\n'])
     yield path_to_file
     os.remove(path_to_file)
